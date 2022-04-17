8512e602ccbce4c1e32845a969a1c83da93a1bc0
NataliaAntipova <100880637+NataliaAntipova@users.noreply.github.com>
2022-04-18 01:57:09 +0300

    Add files via upload

---
+++ 9.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mms_ernet9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            File1.Filter = "(*.jpg)|*.jpg";
+        }
+
+        private void File1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string fname;
+            File1.ShowDialog();
+            fname = File1.FileName;
+            pct.Image = Image.FromFile(fname);
+            MessageBox.Show($"{fname}");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fname;
+            saveFile1.ShowDialog();
+            fname = File1.FileName;
+            pct.Image.Save(fname);
+        }
+    }
+}
---
+++ 9_Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace mms_ernet9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pct = new System.Windows.Forms.PictureBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.File1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFile1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct
+            // 
+            this.pct.Location = new System.Drawing.Point(54, 74);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(257, 167);
+            this.pct.TabIndex = 0;
+            this.pct.TabStop = false;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(54, 272);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(257, 23);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Загрузить изображение";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // File1
+            // 
+            this.File1.FileName = "File1";
+            this.File1.FileOk += new System.ComponentModel.CancelEventHandler(this.File1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.pct);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pct;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.OpenFileDialog File1;
+        private System.Windows.Forms.SaveFileDialog saveFile1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
